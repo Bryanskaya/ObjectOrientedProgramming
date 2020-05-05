12d36754e019156c898eb4bb4507f9422d550391
Bryanskaya <bryanskayakatya@yandex.ru>
2020-05-05 11:55:19 +0300

    ADD: funcs

--- lab_02/vector.cpp
+++ lab_02/vector.cpp
@@ -14,20 +14,30 @@ Vector<Type>::Vector()
 template<typename Type>
 Vector<Type>::Vector(int num)
 {
+    time_t t = time(nullptr);
+
     if (num <= 0)
         throw ErrorSize(__FILE__, typeid (*this).name(), __LINE__,
-                        ctime(&time), num);
+                        ctime(&t), num);
     num_elem = num;
     allocate_memory(num_elem);
+
+    if (!list_elem)
+        throw ErrorMemory(__FILE__, typeid (*this).name(), __LINE__,
+                          ctime(&t));
+
+    // что-то с итератором, пока не поняла что и зачем
 }
 
 template<typename Type>
-Vector<Type>::allocate_memory(int num)
+void Vector<Type>::allocate_memory(int num)
 {
+    time_t t = time(nullptr);
+
     shared_ptr<Type> ptr_temp(new Type[num], default_delete<Type[]>());
     if (!ptr_temp)
         throw ErrorMemory(__FILE__, typeid (*this).name(), __LINE__,
-                          ctime(&time));
+                          ctime(&t));
     list_elem.reset();
     list_elem = ptr_temp;
 }
