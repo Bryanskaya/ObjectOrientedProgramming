a1ef4a5aecec09e8f62585c67fb274ea70f4d6e5
Bryanskaya <bryanskayakatya@yandex.ru>
2020-05-08 00:30:17 +0300

    ADDED: funcs

--- lab_02/iterator.h
+++ lab_02/iterator.h
@@ -15,33 +15,32 @@ template<typename Type>
 class Iterator : public iterator<input_iterator_tag, Type>
 {
     friend class Vector<Type>;
-    Iterator(const shared_ptr<Type[]>& a, const shared_ptr<size_t>& c, size_t ind = 0) :
-        arr(a), count(c), index(ind) {}
+    Iterator(const shared_ptr<Type[]>& a, const shared_ptr<size_t>& c,
+             size_t ind = 0) : arr(a), count(c), index(ind) {}
 
 private:
     weak_ptr<Type[]> arr;
     weak_ptr<size_t> count;
     size_t index = 0;
 
-public:
-    /*Iterator(const shared_ptr<Type[]>& a, const shared_ptr<size_t>& c, size_t ind = 0) :
-        arr(a), count(c), index(ind) {}*/
+    Iterator<Type>& _next();
+    Type get_value();
+    bool _is_end();
 
+public:
     Iterator(const Iterator &iter) = default;
 
+    Iterator<Type>& next();
+    Type value();
+    bool is_end();
+
     Type& operator*();
-    const Type& operator*() const; //
+    const Type& operator*() const;
 
     Type* operator->();
-    const Type* operator->() const; //
+    const Type* operator->() const;
 
     operator bool() const;
-
-    Iterator<Type>& operator+(int n) const; // разобраться с ней
-    Iterator<Type>& operator-(int n) const; // разобраться с ней
-
-    Iterator<Type>& operator+=(int n); //
-    Iterator<Type>& operator-=(int n); //
 
     Iterator<Type>& operator++();
     Iterator<Type> operator++(int);
@@ -49,7 +48,7 @@ public:
     Iterator<Type>& operator--();
     Iterator<Type> operator--(int);
 
-    Iterator<Type>& operator=(const Iterator<Type>& iter); //
+    Iterator<Type>& operator=(const Iterator<Type>& iter);
 
     bool operator==(Iterator const& other) const;
     bool operator!=(Iterator const& other) const;
@@ -62,50 +61,72 @@ public:
 template<typename Type>
 Type& Iterator<Type>::operator*()
 {
+    return get_value();
+}
+
+template<typename Type>
+const Type& Iterator<Type>::operator*() const
+{
     time_t t = time(nullptr);
 
     if (arr.expired())
         throw ErrorNotExist(__FILE__, typeid (*this).name(), __LINE__ - 1, ctime(&t));
+
+    if (index >= count)
+        throw ErrorIndex(__FILE__, typeid (*this).name(), __LINE__ - 1, ctime(&t), index);
+
     shared_ptr<Type[]> a(arr);
 
-    return a[index];
+    return (*a)[index];
 }
 
 template<typename Type>
 Type* Iterator<Type>::operator->()
 {
-    //
+    time_t t = time(nullptr);
+
+    if (arr.expired())
+        throw ErrorNotExist(__FILE__, typeid (*this).name(), __LINE__ - 1, ctime(&t));
+
+    if (index >= count)
+        throw ErrorIndex(__FILE__, typeid (*this).name(), __LINE__ - 1, ctime(&t), index);
+
+    shared_ptr<Type[]> a(arr);
+
+    return &(*a)[index];
+}
+
+template<typename Type>
+const Type* Iterator<Type>::operator->() const
+{
+    time_t t = time(nullptr);
+
+    if (arr.expired())
+        throw ErrorNotExist(__FILE__, typeid (*this).name(), __LINE__ - 1, ctime(&t));
+
+    if (index >= count)
+        throw ErrorIndex(__FILE__, typeid (*this).name(), __LINE__ - 1, ctime(&t), index);
+
+    shared_ptr<Type[]> a(arr);
+
+    return &(*a)[index];
 }
 
 template<typename Type>
 Iterator<Type>& Iterator<Type>::operator=(const Iterator<Type>& iter)
 {
-    //
-}
-
-template<typename Type> // узнать
-Iterator<Type>& Iterator<Type>::operator+=(int n)
-{
-    Iterator<Type> iter(*this);
-    shared_ptr<size_t> n_t(count);
-
-    for (int i = 0; i < n; i++)
-        ++iter;
-
-    if (iter < *n_t)
-        *this = iter;
-
+    index = iter.index;
     return *this;
 }
 
 template<typename Type>
-Iterator<Type>& Iterator<Type>::operator-=(int n)
+Iterator<Type>& Iterator<Type>::next()
 {
-    //
+    return _next();
 }
 
 template<typename Type>
-Iterator<Type>& Iterator<Type>::operator++()
+Iterator<Type>& Iterator<Type>::_next()
 {
     time_t t = time(nullptr);
 
@@ -116,6 +137,46 @@ Iterator<Type>& Iterator<Type>::operator++()
         index++;
 
     return *this;
+}
+
+template<typename Type>
+Type Iterator<Type>::value()
+{
+    return get_value();
+}
+
+template<typename Type>
+Type Iterator<Type>::get_value()
+{
+    time_t t = time(nullptr);
+
+    if (arr.expired())
+        throw ErrorNotExist(__FILE__, typeid (*this).name(), __LINE__ - 1, ctime(&t));
+
+    if (index >= count)
+        throw ErrorIndex(__FILE__, typeid (*this).name(), __LINE__ - 1, ctime(&t), index);
+
+    shared_ptr<Type[]> a(arr);
+
+    return (*a)[index];
+}
+
+template<typename Type>
+bool Iterator<Type>::is_end()
+{
+    return _is_end();
+}
+
+template<typename Type>
+bool Iterator<Type>::_is_end()
+{
+    return index == count;
+}
+
+template<typename Type>
+Iterator<Type>& Iterator<Type>::operator++()
+{    
+    return _next();
 }
 
 template<typename Type>
