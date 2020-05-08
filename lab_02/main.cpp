ff4408efd2709c853529570cf482f0738ceca88d
Bryanskaya <bryanskayakatya@yandex.ru>
2020-05-08 14:18:22 +0300

    CORRECTED: test

--- lab_02/main.cpp
+++ lab_02/main.cpp
@@ -15,12 +15,12 @@ int main()
         //v2.my_print();
 
         cout << "Length v2{-5, 1, 2}, v3{-2.5}: " << v2.get_length() << ' '
-             << v1.get_length() << endl << endl;
+             << v3.get_length() << endl << endl;
         cout << "v4{-9, 2.36, 0, 56, -1.02} v4[0], v4[2], v4[4]: " << ' '
              << v4.get_elem(0) << ' ' << v4.get_elem(2) << ' '
              << v4.get_elem(4) << endl << endl;
         cout << "v4{-9, 2.36, 0, 56, -1.02} v4[0], v4[2], v4[4]: " << ' '
-             << v4[0] << ' ' << v4[2] << ' ' << v4[4] << endl << endl;
+             << v4[100] << ' ' << v4[2] << ' ' << v4[4] << endl << endl;
         cout << "First elements in v2{-5, 1, 2}, v3{-2.5}: " << v2.get_first_elem()
              << ' ' << v3.get_first_elem() << endl << endl;
         cout << "Last elements in v2{-5, 1, 2}, v3{-2.5}: " << v2.get_last_elem()
