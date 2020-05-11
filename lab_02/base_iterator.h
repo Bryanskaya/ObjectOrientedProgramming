90f3ebf112bcb9bcb5da56fbcc4a59be71597091
Bryanskaya <bryanskayakatya@yandex.ru>
2020-05-11 19:30:53 +0300

    ADDED: new file

--- lab_02/base_iterator.h
+++ lab_02/base_iterator.h
@@ -3,7 +3,7 @@
 
 #include <memory>
 
-#include "my_errors.h"
+#include "errors.h"
 
 using namespace std;
 
@@ -20,8 +20,6 @@ public:
 
     BaseIterator<Type>& next();
     bool is_end() const;
-
-    int kate = 10;
 
     operator bool() const;
 
@@ -48,138 +46,13 @@ protected:
     weak_ptr<size_t> count;
     size_t index = 0;
 
-    int help_base = 55;
-
 private:
     BaseIterator<Type>& _next();
     bool _is_end() const;
 };
 
-template<typename Type>
-BaseIterator<Type>& BaseIterator<Type>::operator=(const BaseIterator<Type>& iter)
-{
-    index = iter.index;
-    return *this;
-}
-
-template<typename Type>
-BaseIterator<Type>& BaseIterator<Type>::next()
-{
-    return _next();
-}
-
-template<typename Type>
-BaseIterator<Type>& BaseIterator<Type>::_next()
-{
-    if (count.expired())
-        throw ErrorNotExist(__FILE__, typeid (*this).name(), __LINE__ - 1);
-
-    shared_ptr<size_t> n(count);
-    if (index < *n)
-        index++;
-
-    return *this;
-}
-
-template<typename Type>
-bool BaseIterator<Type>::is_end() const
-{
-    return _is_end();
-}
-
-template<typename Type>
-bool BaseIterator<Type>::_is_end() const
-{
-    if (count.expired())
-        throw ErrorNotExist(__FILE__, typeid (*this).name(), __LINE__ - 1);
-
-    return index == *(count.lock());
-}
-
-template<typename Type>
-BaseIterator<Type>& BaseIterator<Type>::operator++()
-{
-    return _next();
-}
-
-template<typename Type>
-BaseIterator<Type>& BaseIterator<Type>::operator--()
-{
-    if (count.expired())
-        throw ErrorNotExist(__FILE__, typeid (*this).name(), __LINE__ - 1);
-
-    shared_ptr<size_t> n(count);
-    if (index > *n)
-        index--;
-
-    return *this;
-}
-
-template<typename Type>
-BaseIterator<Type> BaseIterator<Type>::operator++(int)
-{
-    BaseIterator<Type> iter(*this);
-
-    ++(*this);
-
-    return iter;
-}
-
-template<typename Type>
-BaseIterator<Type> BaseIterator<Type>::operator--(int)
-{
-    BaseIterator<Type> iter(*this);
-
-    --(*this);
-
-    return iter;
-}
-
-template<typename Type>
-bool BaseIterator<Type>::operator==(BaseIterator const& other) const
-{
-    return index == other.index;
-}
-
-template<typename Type>
-bool BaseIterator<Type>::operator!=(BaseIterator const& other) const
-{
-    return !(index == other.index);
-}
-
-template<typename Type>
-bool BaseIterator<Type>::operator>=(BaseIterator const& other) const
-{
-    return index >= other.index;
-}
-
-template<typename Type>
-bool BaseIterator<Type>::operator>(BaseIterator const& other) const
-{
-    return index > other.index;
-}
-
-template<typename Type>
-bool BaseIterator<Type>::operator<=(BaseIterator const& other) const
-{
-    return index <= other.index;
-}
-
-template<typename Type>
-bool BaseIterator<Type>::operator<(BaseIterator const& other) const
-{
-    return index < other.index;
-}
-
-template<typename Type>
-BaseIterator<Type>::operator bool() const
-{
-    if (count.expired())
-        throw ErrorNotExist(__FILE__, typeid (*this).name(), __LINE__ - 1);
-
-    if (index >= *(count.lock()))
-        return false;
-    return true;
-}
+#ifndef BASE_ITERATOR_HPP_ADVANCED
+#include "base_iterator.hpp"
+#endif // BASE_ITERATOR_HPP_ADVANCED
 
 #endif // BASEITERATOR_H
