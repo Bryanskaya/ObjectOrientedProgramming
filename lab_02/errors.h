e091de80fa1d9549ae6d1261da1b5b300a50705c
Bryanskaya <bryanskayakatya@yandex.ru>
2020-05-14 19:44:25 +0300

    DELETED: comments

--- lab_02/errors.h
+++ lab_02/errors.h
@@ -76,66 +76,6 @@ public:
     virtual ~ErrorNotAllowedSize();
 };
 
-/*class ErrorIndex : public BaseError
-{
-public:
-    ErrorIndex(string filename, string classname, int num_line, int index) :
-        BaseError(filename, classname, num_line,
-                  "Error: wrong index " + to_string(index)){}
 
-    virtual ~ErrorIndex();
-};
-ErrorIndex::~ErrorIndex() = default;
-
-class ErrorMemory : public BaseError
-{
-public:
-    ErrorMemory(string filename, string classname, int num_line) :
-        BaseError(filename, classname, num_line, "Memory error"){}
-
-    virtual ~ErrorMemory();
-};
-ErrorMemory::~ErrorMemory() = default;
-
-class ErrorSize : public BaseError
-{
-public:
-    ErrorSize(string filename, string classname, int num_line, int num) :
-        BaseError(filename, classname, num_line,
-                   "Error: wrong size " + to_string(num)){}
-
-    virtual ~ErrorSize();
-};
-ErrorSize::~ErrorSize() = default;
-
-class ErrorEmpty : public BaseError
-{
-public:
-    ErrorEmpty(string filename, string classname, int num_line) :
-        BaseError(filename, classname, num_line, "Error: vector is empty"){}
-
-    virtual ~ErrorEmpty();
-};
-ErrorEmpty::~ErrorEmpty() = default;
-
-class ErrorNotExist : public BaseError
-{
-public:
-    ErrorNotExist(string filename, string classname, int num_line) :
-        BaseError(filename, classname, num_line, "Error: element not exist"){}
-
-    virtual ~ErrorNotExist();
-};
-ErrorNotExist::~ErrorNotExist() = default;
-
-class ErrorDivZero : public BaseError
-{
-public:
-    ErrorDivZero(string filename, string classname, int num_line) :
-        BaseError(filename, classname, num_line, "Error: division by zero"){}
-
-    virtual ~ErrorDivZero();
-};
-ErrorDivZero::~ErrorDivZero() = default;*/
 
 #endif // MY_ERRORS_H
